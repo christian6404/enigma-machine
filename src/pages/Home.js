e6026399cb18d4fa8b586a040490fec3a9796186
christian6404 <christian6404@gmail.com>
2023-06-13 14:49:43 -0400

    beginings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,29 @@
 import React from "react";
 import "../styles/Home.css";
+import "../components/Plugboard";
+import { plugBoard } from "../components/Plugboard";
+import { rotor1, rotor2, rotor3 } from "../components/Rotors";
 
-var alphabet = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+//TODO:
+//plugBoard needs to have user input change the values
+//rotors need to have the starting position for each rotor not go past 26 and continue to 1 after 26
+//rotors need to have rotation, rotor 1 will rotate
+
+function enigmaMachine(
+  input,
+  config,
+  rotor1Offset,
+  rotor2Offset,
+  rotor3Offset,
+  rotateRotor1,
+  rotateRotor2,
+  rotateRotor3
+) {
+  let inputArr = input.split("").map((el) => plugBoard[el]);
+  return inputArr;
+}
+
+console.log(enigmaMachine("hello world"));
 
 function Home() {
   return (
