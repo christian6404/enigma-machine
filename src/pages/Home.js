af07edf114a4b35e536fedeb7c1d8856e912a09e
christian6404 <christian6404@gmail.com>
2023-06-13 13:55:38 -0400

    input box

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,15 @@ import React from "react";
 import "../styles/Home.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <form>
+        <label for="inputBox">Enter your text:</label>
+        <input type="text" id="inputBox" name="inputBox" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Home;
