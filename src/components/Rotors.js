f31b28de76e644fc043b5c6bfc03c2ce41999166
August Tornea <august.tornea@gmail.com>
2023-06-14 10:01:16 -0400

    adding keyboard

--- src/components/Rotors.js
+++ src/components/Rotors.js
@@ -84,3 +84,125 @@ export const rotor3 = {
   25: 25,
   26: 26,
 };
+
+var Enigma = (function () {
+
+
+  var Rotor,
+    Rotors = [],
+    Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    alphaDecode = {},
+    alphaEncode = {};
+
+
+  return {
+    Initialise: function () {
+      // Defining the Rotor function and its methods
+      Rotor = (function () {
+        class Rotor {
+          constructor(rotorSettings, turnover, startPosition) {
+            this.id = Rotors.length;
+            this.currentPosition = 0;
+            this.currentReversePosition = 0;
+            this.turnover = turnover;
+            this.mapLength = rotorSettings.length;
+            this.element = '';
+
+            if (startPosition) {
+              this.currentPosition = startPosition;
+              this.currentReversePosition = parseInt('-' + startPosition);
+            }
+
+            this.map = {};
+
+            this.reverse = {};
+
+            this.setMap(rotorSettings);
+            document.body.addEventListener('tickOver', this.tickOver);
+            this.element.onchange = Enigma.getRotorInput;
+
+            if (startPosition) {
+              this.element.value = this.pad(this.currentPosition + 1);
+            }
+          }
+          setMap(rotorSettings) {
+            if (rotorSettings !== undefined) {
+              for (var i = 0; i < rotorSettings.length; i++) {
+                var mapLetter = Alphabet[i];
+                this.map[mapLetter] = rotorSettings[i];
+                this.reverse[rotorSettings[i]] = mapLetter;
+              }
+            }
+
+            this.element = document.getElementById('rotor_' + this.id);
+          }
+          tick() {
+            if (this.mapLength - 1 === this.currentPosition) {
+              this.currentPosition = 0;
+              this.currentReversePosition = 0;
+            } else {
+              this.currentPosition++;
+              this.currentReversePosition--;
+            }
+
+            if (this.currentPosition === this.turnover) {
+              Enigma.Trigger('tickOver', this.id);
+            }
+
+            this.element.value = this.pad(this.currentPosition + 1);
+            this.element.classList.add('run');
+
+            return this.currentPosition;
+          }
+          setRotor(val) {
+            if (val === 0) {
+              this.currentPosition = 0;
+              this.currentReversePosition = 0;
+            } else {
+              this.currentPosition = val;
+              this.currentReversePosition = parseInt('-' + val);
+            }
+
+            this.element.value = this.pad(this.currentPosition + 1);
+            this.element.classList.add('run');
+            Enigma.ShiftRotors();
+          }
+          tickOver(request) {
+            if (Rotors[request.detail + 1] !== undefined) {
+              Rotors[request.detail + 1].tick();
+            }
+          }
+          convertLetter(letter, reverse) {
+            if (reverse) {
+              letter = Enigma.ReverseOffset(letter, this.currentReversePosition);
+              letter = alphaDecode[this.reverse[alphaEncode[letter]]];
+              return Enigma.ReverseOffset(letter, this.currentReversePosition);
+            } else {
+              letter = Enigma.Offset(letter, this.currentPosition);
+              letter = alphaDecode[this.map[alphaEncode[letter]]];
+              return Enigma.Offset(letter, this.currentPosition);
+            }
+          }
+          pad(val) {
+            val += '';
+            return val.length === 2 ? val : '0' + val;
+          }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return Rotor;
+      })();
+    },
+  };
+})
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,6 @@ import "../components/Plugboard";
 import { plugBoard } from "../components/Plugboard";
 import { rotor1, rotor2, rotor3 } from "../components/Rotors";
 
-//TODO:
-//plugBoard needs to have user input change the values
-//rotors need to have the starting position for each rotor not go past 26 and continue to 1 after 26
-//rotors need to have rotation, rotor 1 will rotate
-
 function enigmaMachine(
   input,
   config,
@@ -25,13 +20,73 @@ function enigmaMachine(
 
 console.log(enigmaMachine("hello world"));
 
+const EnigmaOutput = () => {
+  return (
+    <div className="output">
+      <button id="flick" name="flick" type="button" aria-label="Change Output View"></button>
+
+      <ol className="lamps">
+        <li className="q">q</li>
+        <li className="w">w</li>
+        <li className="e">e</li>
+        <li className="r">r</li>
+        <li className="t">t</li>
+        <li className="y">y</li>
+        <li className="u">u</li>
+        <li className="i">i</li>
+        <li className="o">o</li>
+        <li className="p">p</li>
+
+        <li className="space"></li>
+
+        <li className="a">a</li>
+        <li className="s">s</li>
+        <li className="d">d</li>
+        <li className="f">f</li>
+        <li className="g">g</li>
+        <li className="h">h</li>
+        <li className="j">j</li>
+        <li className="k">k</li>
+        <li className="l">l</li>
+
+        <li className="space"></li>
+
+        <li className="z">z</li>
+        <li className="x">x</li>
+        <li className="c">c</li>
+        <li className="v">v</li>
+        <li className="b">b</li>
+        <li className="n">n</li>
+        <li className="m">m</li>
+      </ol>
+
+      <div className="encrypted" id="output"></div>
+    </div>
+  );
+};
+
 function Home() {
   return (
     <div className="Home">
       <form>
-        <label for="inputBox">Enter your text:</label>
+        <div className="rotors">
+          <input type="text" maxLength="2" id="rotor_2" value="01" autoComplete="off" />
+          <span className="cog"></span>
+
+          <input type="text" maxLength="2" id="rotor_1" value="01" autoComplete="off" />
+          <span className="cog"></span>
+
+          <input type="text" maxLength="2" id="rotor_0" value="01" autoComplete="off" />
+          <span className="cog"></span>
+
+          <button className="set" id="set" type="button">Set</button>
+        </div>
+
+        <label htmlFor="inputBox">Enter your text:</label>
         <input type="text" id="inputBox" name="inputBox" />
         <input type="submit" value="Submit" />
+
+        <EnigmaOutput />
       </form>
     </div>
   );
